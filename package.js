22922f8d98e74bbc688ef36067cd38b9db754900
Philippe Beck <philippe@philippebeck.net>
2019-03-08 21:43:06 +0100

    Replace all quotes again for meteor

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
-  name: 'animadio:animadio',
-  version: '0.1.14',
-  summary: 'Animadio CSS Framework - Grid & Flexbox Layouts - Reusable & Customizable Elements',
-  git: 'https://github.com/animadio/animadio.git',
-  documentation: 'README.md'
+  name: "animadio:animadio",
+  version: "0.1.14",
+  summary: "Animadio CSS Framework - Grid & Flexbox Layouts - Reusable & Customizable Elements",
+  git: "https://github.com/animadio/animadio.git",
+  documentation: "README.md"
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('METEOR@1.0');
+  api.versionsFrom("METEOR@1.0");
   api.addFiles([
-    'dist/animadio.css'
-  ], 'client');
+    "dist/animadio.css"
+  ], "client");
 });
