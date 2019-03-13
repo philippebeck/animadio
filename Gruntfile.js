613acb163b600f51cb2b15b7691ddf224134e153
Philippe Beck <philippe@philippebeck.net>
2019-03-13 12:56:22 +0100

    Rewrite gruntfile for maintainability

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,53 +11,11 @@ module.exports = function(grunt) {
             dist: {
                 src: [
                     "node_modules/normalize.css/normalize.css",
-                    /***** Global *****/
-                    "src/global/tags.css",
-                    "src/global/grid.css",
-                    "src/global/flex.css",
-                    "src/global/display.css",
-                    "src/global/color.css",
-                    "src/global/space.css",
-                    "src/global/bord.css",
-                    "src/global/size.css",
-                    "src/global/shabox.css",
-                    "src/global/shatex.css",
-                    "src/global/font.css",
-                    /***** Animations *****/
-                    "src/animations/keyframes.css",
-                    "src/animations/anima.css",
-                    "src/animations/options.css",
-                    "src/animations/origin.css",
-                    /***** Elements *****/
-                    "src/elements/navbar.css",
-                    "src/elements/button.css",
-                    "src/elements/slider.css",
-                    "src/elements/menu.css",
-                    "src/elements/gallery.css",
-                    "src/elements/table.css",
-                    "src/elements/form.css",
-                    "src/elements/footer.css"
-                ],
-                dest: "dist/animadio.css"
-            }
-        },
-        postcss: {
-            options: {
-                processors: [
-                    require("autoprefixer")({browsers: "last 2 versions"})
-                ]
-            },
-            dist: {
-                src: "dist/animadio.css"
-            }
-        },
-        cssmin: {
-            target: {
-                files: [{
-                    "dist/min/animadio.min.css": ["dist/animadio.css"]
-                }]
-            }
-        }
+                    "src/global/keyframes.css", "src/global/tags.css", "src/global/grid.css", "src/global/flex.css", "src/global/display.css", "src/global/color.css", "src/global/space.css", "src/global/bord.css", "src/global/size.css", "src/global/shabox.css", "src/global/shatex.css", "src/global/anima.css", "src/global/font.css",
+                    "src/elements/navbar.css", "src/elements/button.css", "src/elements/slider.css", "src/elements/menu.css", "src/elements/gallery.css", "src/elements/table.css", "src/elements/form.css", "src/elements/footer.css" ],
+                dest: "dist/animadio.css"}},
+        postcss: {options: {rocessors: [require("autoprefixer")({browsers: "last 2 versions"})]}, dist: {src: "dist/animadio.css"}},
+        cssmin: {target: {files: [{"dist/min/animadio.min.css": ["dist/animadio.css"]}]}}
     });
     grunt.loadNpmTasks("grunt-contrib-concat");
     grunt.loadNpmTasks("grunt-postcss");
