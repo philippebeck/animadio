2abdb36fc5469c26e5c44d4e219424a1db310a60
Philippe Beck <philippe@philippebeck.net>
2019-03-13 13:02:25 +0100

    Correct typo to gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,7 +14,7 @@ module.exports = function(grunt) {
                     "src/global/keyframes.css", "src/global/tags.css", "src/global/grid.css", "src/global/flex.css", "src/global/display.css", "src/global/color.css", "src/global/space.css", "src/global/bord.css", "src/global/size.css", "src/global/shabox.css", "src/global/shatex.css", "src/global/anima.css", "src/global/font.css",
                     "src/elements/navbar.css", "src/elements/button.css", "src/elements/slider.css", "src/elements/menu.css", "src/elements/gallery.css", "src/elements/table.css", "src/elements/form.css", "src/elements/footer.css" ],
                 dest: "dist/animadio.css"}},
-        postcss: {options: {rocessors: [require("autoprefixer")({browsers: "last 2 versions"})]}, dist: {src: "dist/animadio.css"}},
+        postcss: {options: {processors: [require("autoprefixer")({browsers: "last 2 versions"})]}, dist: {src: "dist/animadio.css"}},
         cssmin: {target: {files: [{"dist/min/animadio.min.css": ["dist/animadio.css"]}]}}
     });
     grunt.loadNpmTasks("grunt-contrib-concat");
