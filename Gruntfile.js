588cf5b02e198207f67a91a998a48199f307bfbd
Philippe Beck <philippe@philippebeck.net>
2019-03-19 23:18:21 +0100

    Fix the grunt file for new structure

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,10 +9,7 @@ module.exports = function(grunt) {
                 footer: "\n/* Author: <%= pkg.author.name %> <<%= pkg.author.email %>>\n Updated: <%= grunt.template.today('dS mmm yyyy @ h:MM:ss TT') %> */"
             },
             dist: {
-                src: [
-                    "node_modules/normalize.css/normalize.css",
-                    "src/global/keyframes.css", "src/global/tags.css", "src/global/display.css", "src/global/grid.css", "src/global/flex.css", "src/global/position.css", "src/global/flow.css", "src/global/color.css", "src/global/space.css", "src/global/bord.css", "src/global/size.css", "src/global/shabox.css", "src/global/shatex.css", "src/global/anima.css", "src/global/deco.css", "src/global/helpers.css",
-                    "src/elements/navbar.css", "src/elements/btn.css", "src/elements/slider.css", "src/elements/menu.css", "src/elements/gallery.css", "src/elements/table.css", "src/elements/form.css", "src/elements/foot.css" ],
+                src: ["node_modules/normalize.css/normalize.css", "src/global/keyframes.css", "src/global/tags.css", "src/global/space.css", "src/global/size.css", "src/global/helpers.css", "src/medias/grid.css", "src/medias/flex.css", "src/poly/display.css", "src/poly/position.css", "src/poly/flow.css", "src/concat/color.css", "src/concat/shabox.css", "src/concat/shatex.css", "src/states/bord.css", "src/states/anima.css", "src/states/deco.css", "src/elements/navbar.css", "src/elements/btn.css", "src/elements/slider.css", "src/elements/menu.css", "src/elements/gallery.css", "src/elements/table.css", "src/elements/form.css", "src/elements/foot.css"],
                 dest: "dist/animadio.css"}},
         postcss: {options: {processors: [require("autoprefixer")({browsers: "last 2 versions"})]}, dist: {src: "dist/animadio.css"}},
         cssmin: {target: {files: [{"dist/min/animadio.min.css": ["dist/animadio.css"]}]}}
