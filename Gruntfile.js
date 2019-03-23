69315738b0d14de22e38a9f5a0699e132e7b10e4
Philippe Beck <philippe@philippebeck.net>
2019-03-23 17:23:18 +0100

    Add exclamation point for keep header & footer comments

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,8 +5,8 @@ module.exports = function(grunt) {
             options: {
                 sourceMap: true,
                 sourceMapName: "dist/map/animadio.css.map",
-                banner: "/* <%= pkg.name %> v<%= pkg.version %> | <%= pkg.homepage %> | <%= pkg.license %> License */\n\n",
-                footer: "\n/* Author: <%= pkg.author.name %> <<%= pkg.author.email %>>\n Updated: <%= grunt.template.today('dS mmm yyyy @ h:MM:ss TT') %> */"
+                banner: "/*! <%= pkg.name %> v<%= pkg.version %> | <%= pkg.homepage %> | <%= pkg.license %> License */\n\n",
+                footer: "\n/*! Author: <%= pkg.author.name %> <<%= pkg.author.email %>>\n Updated: <%= grunt.template.today('dS mmm yyyy @ h:MM:ss TT') %> */"
             },
             dist: {
                 src: ["node_modules/normalize.css/normalize.css", "src/global/keyframes.css", "src/global/tags.css", "src/global/space.css", "src/global/size.css", "src/global/helpers.css", "src/medias/grid.css", "src/medias/flex.css", "src/medias/align.css", "src/medias/justify.css", "src/poly/display.css", "src/poly/position.css", "src/poly/flow.css", "src/concat/color.css", "src/concat/shabox.css", "src/concat/shatex.css", "src/states/bord.css", "src/states/anima.css", "src/states/deco.css", "src/elements/navbar.css", "src/elements/btn.css", "src/elements/slider.css", "src/elements/menu.css", "src/elements/gallery.css", "src/elements/table.css", "src/elements/form.css", "src/elements/foot.css"],
