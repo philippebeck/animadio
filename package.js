a4662ac15b740db00d7e08b1f6c1368f91cc276d
Philippe Beck <philippe@philippebeck.net>
2019-04-03 17:25:54 +0200

    Release 0.2.16

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "animadio:animadio",
-  version: "0.2.15",
+  version: "0.2.16",
   summary: "Animadio CSS Framework - Grid & Flexbox Layouts - Reusable & Customizable Elements",
   git: "https://github.com/animadio/animadio.git",
   documentation: "README.md"
