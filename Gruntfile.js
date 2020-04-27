41d911a5c12aa71f53d56331f3e6c0a9bc946a2f
Philippe Beck <philippe@philippebeck.net>
2020-04-27 20:12:51 +0200

    Update gruntfile for the 2 new parts

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,11 +4,13 @@ module.exports = function(grunt) {
         concat: {
             options: {banner: "/*! <%= pkg.name %> v<%= pkg.version %> | <%= pkg.homepage %> | <%= pkg.license %> License */\n\n", footer: "\n/*! Author: <%= pkg.author.name %> <<%= pkg.author.email %>>\n Updated: <%= grunt.template.today('dS mmm yyyy @ h:MM:ss TT') %> */"},
             main: {src: ["src/css/animadio.css"], dest: "dist/animadio.css"},
+            box: {src: ["src/css/animadio-box.css"], dest: "dist/animadio-box.css"},
             grid: {src: ["src/css/animadio-grid.css"], dest: "dist/animadio-grid.css"},
             elements: {src: ["src/css/animadio-elements.css"], dest: "dist/animadio-elements.css"},
-            states: {src: ["src/css/animadio-states.css"], dest: "dist/animadio-states.css"}},
-        postcss: {options: {processors: [require("autoprefixer")({browsers: "last 2 versions"})]}, main: {src: "dist/animadio.css"}, grid: {src: "dist/animadio-grid.css"}, elements: {src: "dist/animadio-elements.css"}, states: {src: "dist/animadio-states.css"}},
-        cssmin: {target: {files: [{"dist/min/animadio.min.css": ["dist/animadio.css"]}, {"dist/min/animadio-grid.min.css": ["dist/animadio-variables.css"]}, {"dist/min/animadio-elements.min.css": ["dist/animadio-variables.css"]}, {"dist/min/animadio-states.min.css": ["dist/animadio-variables.css"]}]}}
+            states: {src: ["src/css/animadio-states.css"], dest: "dist/animadio-states.css"},
+            helpers: {src: ["src/css/animadio-helpers.css"], dest: "dist/animadio-helpers.css"}},
+        postcss: {options: {processors: [require("autoprefixer")({browsers: "last 2 versions"})]}, main: {src: "dist/animadio.css"}, box: {src: "dist/animadio-box.css"}, grid: {src: "dist/animadio-grid.css"}, elements: {src: "dist/animadio-elements.css"}, states: {src: "dist/animadio-states.css"}}, helpers: {src: "dist/animadio-helpers.css"},
+        cssmin: {target: {files: [{"dist/min/animadio.min.css": ["dist/animadio.css"]}, {"dist/min/animadio-box.min.css": ["dist/animadio-box.css"]}, {"dist/min/animadio-grid.min.css": ["dist/animadio-grid.css"]}, {"dist/min/animadio-elements.min.css": ["dist/animadio-elements.css"]}, {"dist/min/animadio-states.min.css": ["dist/animadio-states.css"]}, {"dist/min/animadio-helpers.min.css": ["dist/animadio-helpers.css"]}]}}
     });
     grunt.loadNpmTasks("grunt-contrib-concat");
     grunt.loadNpmTasks("grunt-postcss");
