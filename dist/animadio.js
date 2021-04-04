6fc692d609336a989ee3e09af1a4d94868dd79ab
Philippe Beck <philippe@philippebeck.net>
2021-04-04 13:19:07 +0200

    Release 4.2.0

--- dist/animadio.js
+++ dist/animadio.js
@@ -1,4 +1,4 @@
-/*! animadio v4.1.10 | https://animadio.org | MIT License */
+/*! animadio v4.2.0 | https://animadio.org | MIT License */
 
 "use strict";
 
@@ -462,4 +462,4 @@ class Ajax {
 }
 
 /*! Author: Philippe Beck <philippe@philippebeck.net>
- Updated: 3rd Apr 2021 @ 7:41:29 PM */
+ Updated: 4th Apr 2021 @ 12:50:33 PM */
