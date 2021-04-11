bd654d73be2219ebf3236f6c8a97440f65a426ac
Philippe Beck <philippe@philippebeck.net>
2021-04-11 10:40:16 +0200

    Fix canvas JS for naming & refactoring methods

--- src/js/Canvas.js
+++ src/js/Canvas.js
@@ -8,14 +8,10 @@ class Canvas {
   constructor(canvasWidth = 500, 
               canvasHeight = 500, 
               lineWidth = 2, 
-              lineColor = "blue") {
+              lineColor = "black") {
 
-    this.canvasElt    = null;
-    this.lineWidthElt = null;
-    this.lineColorElt = null;
-    this.cleanerElt   = null;
-
-    this.context = null;
+    this.canvasElt  = null;
+    this.context    = null;
 
     this.startState = false;
     this.endState   = false;
@@ -25,80 +21,40 @@ class Canvas {
     this.touchX = 0;
     this.touchY = 0;
 
-    this.getCanvasElt();
-    this.getOptionsElt();
+    this.lineWidthElt = null;
+    this.lineColorElt = null;
+    this.cleanerElt   = null;
 
-    this.setCanvasDimensions(canvasWidth, canvasHeight);
-    this.setContext2d(lineWidth, lineColor);
-
-    this.setCanvasListeners();
-    this.setOptionsListeners();
+    this.setCanvas(canvasWidth, canvasHeight);
+    this.setContext(lineWidth, lineColor);
+    this.setOptions();
+    this.setListeners();
   }
 
-  /*****************************/
-  /********** GETTERS **********/
-  /*****************************/
+  /**********************************/
+  /********** INIT SETTERS **********/
+  /**********************************/
 
-  getCanvasElt() {
+  /**
+   * @param {number} width
+   * @param {number} height
+   */
+  setCanvas(width, height) {
     if (document.getElementById("canvas")) {
-      this.canvasElt = document.getElementById("canvas");
+      this.canvasElt        = document.getElementById("canvas");
+      this.canvasElt.width  = width;
+      this.canvasElt.height = height;
 
     } else {
       alert("No <canvas id=\"canvas\"> element is present!");
     }
   }
 
-  getOptionsElt() {
-    if (document.getElementById("line-color")) {
-      this.lineColorElt = document.getElementById("line-color");
-    }
-
-    if (document.getElementById("line-width")) {
-      this.lineWidthElt = document.getElementById("line-width");
-    }
-
-    if (document.getElementById("cleaner")) {
-      this.cleanerElt = document.getElementById("cleaner");
-    }
-  }
-
-  /**
-   * @param {object} event
-   */
-   getMouseLocation(event) {
-    this.mouseX = event.offsetX;
-    this.mouseY = event.offsetY;
-  }
-
-  /**
-   * @param {object} event
-   */
-  getTouchLocation(event) {
-    let position  = event.target.getBoundingClientRect();
-    this.touchX   = event.targetTouches[0].clientX - position.left;
-    this.touchY   = event.targetTouches[0].clientY - position.top;
-  }
-
-  /*****************************/
-  /********** SETTERS **********/
-  /*****************************/
-
-  /**
-   * @param {number} width
-   * @param {number} height
-   */
-  setCanvasDimensions(width, height) {
-    if (this.canvasElt) {
-      this.canvasElt.width   = width;
-      this.canvasElt.height  = height;
-    } 
-  }
-
   /**
    * @param {number} lineWidth
    * @param {string} strokeStyle
    */
-  setContext2d(lineWidth, strokeStyle) {
+  setContext(lineWidth, strokeStyle) {
     if (this.canvasElt.getContext) {
       this.context = this.canvasElt.getContext("2d");
 
@@ -112,7 +68,21 @@ class Canvas {
     }
   }
 
-  setCanvasListeners() {
+  setOptions() {
+    if (document.getElementById("line-color")) {
+      this.lineColorElt = document.getElementById("line-color");
+    }
+
+    if (document.getElementById("line-width")) {
+      this.lineWidthElt = document.getElementById("line-width");
+    }
+
+    if (document.getElementById("cleaner")) {
+      this.cleanerElt = document.getElementById("cleaner");
+    }
+  }
+
+  setListeners() {
     if (this.canvasElt) {
  
      this.canvasElt.addEventListener("mousedown", this.moveInCanvas.bind(this, "mouse"));
@@ -125,9 +95,7 @@ class Canvas {
      this.canvasElt.addEventListener("touchend", this.stopDrawing.bind(this));
      this.canvasElt.addEventListener("touchcancel", this.stopDrawing.bind(this));
     }
-   }
 
-  setOptionsListeners() {
     if (this.lineWidthElt) {
       this.lineWidthElt.addEventListener("input", this.setLineWidth.bind(this));
     }
@@ -141,9 +109,9 @@ class Canvas {
     }
   }
 
-  /*******************************/
-  /********** LISTENERS **********/
-  /*******************************/
+  /************************************/
+  /********** EVENTS METHODS **********/
+  /************************************/
 
   /**
    * @param {string} type
@@ -154,12 +122,12 @@ class Canvas {
     switch (type) {
 
       case "mouse":
-        this.getMouseLocation(event);
+        this.setMouseLocation(event);
         this.moveTo(event, this.mouseX, this.mouseY);
         break;
 
       case "touch":
-        this.getTouchLocation(event);
+        this.setTouchLocation(event);
         this.moveTo(event, this.touchX, this.touchY);
         break;
 
@@ -177,12 +145,12 @@ class Canvas {
       switch (type) {
 
         case "mouse":
-          this.getMouseLocation(event);
+          this.setMouseLocation(event);
           this.lineTo(this.mouseX, this.mouseY);
           break;
 
         case "touch":
-          this.getTouchLocation(event);
+          this.setTouchLocation(event);
           this.lineTo(this.touchX, this.touchY);
           break;
 
@@ -208,6 +176,27 @@ class Canvas {
   clearCanvas() {
     this.context.clearRect(0, 0, this.canvasElt.width, this.canvasElt.height);
     this.endState = false;
+  }
+
+  /**************************************/
+  /********** LOCATION SETTERS **********/
+  /**************************************/
+
+  /**
+   * @param {object} event
+   */
+  setMouseLocation(event) {
+    this.mouseX = event.offsetX;
+    this.mouseY = event.offsetY;
+  }
+
+  /**
+   * @param {object} event
+   */
+  setTouchLocation(event) {
+    let position  = event.target.getBoundingClientRect();
+    this.touchX   = event.targetTouches[0].clientX - position.left;
+    this.touchY   = event.targetTouches[0].clientY - position.top;
   }
 
   /*************************************/
