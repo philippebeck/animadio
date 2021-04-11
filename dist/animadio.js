f08aa20faa9482639f167c6d6f4c7338ce1789b7
Philippe Beck <philippe@philippebeck.net>
2021-04-11 12:59:15 +0200

    Version 4.2.8

--- dist/animadio.js
+++ dist/animadio.js
@@ -1,4 +1,4 @@
-/*! animadio v4.2.7 | https://animadio.org | MIT License */
+/*! animadio v4.2.8 | https://animadio.org | MIT License */
 
 "use strict";
 
@@ -24,70 +24,141 @@ class Slider {
    * @param {Boolean} auto
    * @param {Boolean} random
    */
-  constructor(timeout = 2000, auto = true, random = false) {
-    this.slider         = document.getElementById("slider");
-    this.slidesTriggers = this.slider.querySelectorAll("input");
-    this.slidesCount    = this.slidesTriggers.length;
+  constructor(timeout = 2000, 
+              auto = true, 
+              random = false) {
 
-    this.previous = document.getElementById("slider-previous");
-    this.next     = document.getElementById("slider-next");
+    this.sliderElt      = null;
+    this.slidesTriggers = null;
+    this.slidesCount    = 0;
 
     this.index    = -1;
     this.timer    = null;
     this.timeout  = timeout;
 
-    this.auto       = document.getElementById("slider-auto");
-    this.autoIcon   = this.auto.querySelector("i");
+    this.previousElt = null;
+    this.nextElt     = null;
+
+    this.autoElt    = null;
+    this.autoIcon   = null;
     this.autoState  = auto;
 
-    this.random       = document.getElementById("slider-random");
-    this.randomIcon   = this.random.querySelector("i");
+    this.randomElt    = null;
+    this.randomIcon   = null;
     this.randomState  = random;
 
-    this.setControls();
+    this.setSlider();
+    this.setSlidesTriggers();
+    this.setOptions();
+    this.setIcons();
+    this.setListeners();
+
     this.runSlider();
   }
 
-  setControls() {
-    this.previous.addEventListener("click", this.goPrevious.bind(this));
-    this.next.addEventListener("click", this.goNext.bind(this));
+  /**********************************/
+  /********** INIT METHODS **********/
+  /**********************************/
 
-    this.auto.addEventListener("click", this.checkAuto.bind(this));
-    this.random.addEventListener("click", this.checkRandom.bind(this));
+  setSlider() {
+    if (document.getElementById("slider")) {
+      this.sliderElt = document.getElementById("slider");
 
+    } else {
+      alert("No <tag id=\"slider\"> element is present!");
+    } 
+  }
+
+  setSlidesTriggers() {
+    if (this.sliderElt.querySelectorAll("input")) {
+      this.slidesTriggers = this.sliderElt.querySelectorAll("input");
+      this.slidesCount    = this.slidesTriggers.length;
+    } 
+  }
+
+  setOptions() {
+    if (document.getElementById("slider-previous")) {
+      this.previousElt = document.getElementById("slider-previous");
+    }
+    
+    if (document.getElementById("slider-next")) {
+      this.nextElt = document.getElementById("slider-next");
+    }
+
+    if (document.getElementById("slider-auto")) {
+      this.autoElt = document.getElementById("slider-auto");
+    }
+  
+    if (document.getElementById("slider-random")) {
+      this.randomElt = document.getElementById("slider-random");
+    }
+  }
+
+  setIcons() {
+    if (this.autoElt.querySelector("i")) {
+      this.autoIcon = this.autoElt.querySelector("i");
+    }
+
+    if (this.randomElt.querySelector("i")) {
+      this.randomIcon = this.randomElt.querySelector("i");
+    }
+  }
+
+  setListeners() {
     document.addEventListener("keydown", this.setKeyboard.bind(this));
+
+    if (this.previousElt) {
+      this.previousElt.addEventListener("click", this.goPrevious.bind(this));
+    }
+
+    if (this.nextElt) {
+      this.nextElt.addEventListener("click", this.goNext.bind(this));
+    }
+
+    if (this.autoElt) {
+      this.autoElt.addEventListener("click", this.checkAuto.bind(this));
+    }
+
+    if (this.randomElt) {
+      this.randomElt.addEventListener("click", this.checkRandom.bind(this));
+    }
   }
 
   runSlider() {
     if (this.autoState) {
-      this.timer = window.setInterval(
-          this.goNext.bind(this),
-          this.timeout
-      );
+      this.timer = window.setInterval(this.goNext.bind(this), this.timeout);
 
     } else {
       this.goNext();
     }
   }
 
+  /************************************/
+  /********** EVENTS METHODS **********/
+  /************************************/
+
   /**
-   * @param {number} min
-   * @param {number} max
-   * @return
+   * @param {Object} event
    */
-  getRandomInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
+  setKeyboard(event) {
+    switch (event.code) {
 
-  refreshSlide() {
-    for (let i = 0; i < this.slidesCount; i++) {
+      case "ArrowLeft":
+        this.goPrevious();
+        break;
 
-      if (this.slidesTriggers[i].hasAttribute("checked")) {
-        this.slidesTriggers[i].removeAttribute("checked");
-      }
+      case "ArrowUp":
+        this.checkAuto();
+        break;
+
+      case "ArrowDown":
+        this.checkRandom();
+        break;
+
+      case "ArrowRight":
+        this.goNext();
+        break;
     }
-
-    this.slidesTriggers[this.index].setAttribute("checked", true);
   }
 
   goPrevious() {
@@ -133,19 +204,6 @@ class Slider {
     this.refreshSlide();
   }
 
-  /**
-   * @param {boolean} state
-   * @param {string} title
-   * @param {string} add
-   * @param {string} remove
-   */
-  setAuto(state, title, add, remove) {
-    this.autoState  = state;
-    this.auto.title = title;
-
-    this.setIcon(this.autoIcon, add, remove);
-  }
-
   checkRandom() {
     if (this.randomState) {
       this.setRandom(false, "Random", "fa-random", "fa-long-arrow-alt-right");
@@ -157,6 +215,48 @@ class Slider {
     this.refreshSlide();
   }
 
+  refreshSlide() {
+    for (let i = 0; i < this.slidesCount; i++) {
+
+      if (this.slidesTriggers[i].hasAttribute("checked")) {
+        this.slidesTriggers[i].removeAttribute("checked");
+      }
+    }
+
+    this.slidesTriggers[this.index].setAttribute("checked", true);
+  }
+
+  /****************************/
+  /********** GETTER **********/
+  /****************************/
+
+  /**
+   * @param {number} min
+   * @param {number} max
+   * @return
+   */
+   getRandomInteger(min, max) {
+
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  /*************************************/
+  /********** OPTIONS SETTERS **********/
+  /*************************************/
+
+  /**
+   * @param {boolean} state
+   * @param {string} title
+   * @param {string} add
+   * @param {string} remove
+   */
+  setAuto(state, title, add, remove) {
+    this.autoState      = state;
+    this.autoElt.title  = title;
+
+    this.setIcon(this.autoIcon, add, remove);
+  }
+
   /**
    * @param {boolean} state
    * @param {string} title
@@ -164,8 +264,8 @@ class Slider {
    * @param {string} remove
    */
   setRandom(state, title, add, remove) {
-    this.randomState  = state;
-    this.random.title = title;
+    this.randomState      = state;
+    this.randomElt.title  = title;
 
     this.setIcon(this.randomIcon, add, remove);
   }
@@ -179,30 +279,6 @@ class Slider {
     icon.classList.add(add);
     icon.classList.remove(remove);
   }
-
-  /**
-   * @param {Object} event
-   */
-  setKeyboard(event) {
-    switch (event.code) {
-
-      case "ArrowLeft":
-        this.goPrevious();
-        break;
-
-      case "ArrowUp":
-        this.checkAuto();
-        break;
-
-      case "ArrowDown":
-        this.checkRandom();
-        break;
-
-      case "ArrowRight":
-        this.goNext();
-        break;
-    }
-  }
 }
 
 class Canvas {
@@ -215,14 +291,10 @@ class Canvas {
   constructor(canvasWidth = 500, 
               canvasHeight = 500, 
               lineWidth = 2, 
-              lineColor = "blue") {
+              lineColor = "black") {
 
-    this.canvasElt    = null;
-    this.lineWidthElt = null;
-    this.lineColorElt = null;
-    this.cleanerElt   = null;
-
-    this.context = null;
+    this.canvasElt  = null;
+    this.context    = null;
 
     this.startState = false;
     this.endState   = false;
@@ -232,80 +304,40 @@ class Canvas {
     this.touchX = 0;
     this.touchY = 0;
 
-    this.getCanvasElt();
-    this.getOptionsElt();
+    this.lineWidthElt = null;
+    this.lineColorElt = null;
+    this.cleanerElt   = null;
 
-    this.setCanvasDimensions(canvasWidth, canvasHeight);
-    this.setContext2d(lineWidth, lineColor);
-    
-    this.setCanvasListeners();
-    this.setOptionsListeners();
+    this.setCanvas(canvasWidth, canvasHeight);
+    this.setContext(lineWidth, lineColor);
+    this.setOptions();
+    this.setListeners();
   }
 
-  /*****************************/
-  /********** GETTERS **********/
-  /*****************************/
+  /**********************************/
+  /********** INIT SETTERS **********/
+  /**********************************/
 
-  getCanvasElt() {
+  /**
+   * @param {number} width
+   * @param {number} height
+   */
+  setCanvas(width, height) {
     if (document.getElementById("canvas")) {
-      this.canvasElt = document.getElementById("canvas");
+      this.canvasElt        = document.getElementById("canvas");
+      this.canvasElt.width  = width;
+      this.canvasElt.height = height;
 
     } else {
       alert("No <canvas id=\"canvas\"> element is present!");
     }
   }
 
-  getOptionsElt() {
-    if (document.getElementById("line-color")) {
-      this.lineColorElt = document.getElementById("line-color");
-    }
-
-    if (document.getElementById("line-width")) {
-      this.lineWidthElt = document.getElementById("line-width");
-    }
-
-    if (document.getElementById("cleaner")) {
-      this.cleanerElt = document.getElementById("cleaner");
-    }
-  }
-
-  /**
-   * @param {object} event
-   */
-   getMouseLocation(event) {
-    this.mouseX = event.offsetX;
-    this.mouseY = event.offsetY;
-  }
-
-  /**
-   * @param {object} event
-   */
-  getTouchLocation(event) {
-    let position  = event.target.getBoundingClientRect();
-    this.touchX   = event.targetTouches[0].clientX - position.left;
-    this.touchY   = event.targetTouches[0].clientY - position.top;
-  }
-
-  /*****************************/
-  /********** SETTERS **********/
-  /*****************************/
-
-  /**
-   * @param {number} width
-   * @param {number} height
-   */
-  setCanvasDimensions(width, height) {
-    if (this.canvasElt) {
-      this.canvasElt.width   = width;
-      this.canvasElt.height  = height;
-    } 
-  }
-
   /**
    * @param {number} lineWidth
    * @param {string} strokeStyle
    */
-  setContext2d(lineWidth, strokeStyle) {
+  setContext(lineWidth, strokeStyle) {
     if (this.canvasElt.getContext) {
       this.context = this.canvasElt.getContext("2d");
 
@@ -319,7 +351,21 @@ class Canvas {
     }
   }
 
-  setCanvasListeners() {
+  setOptions() {
+    if (document.getElementById("canvas-line-color")) {
+      this.lineColorElt = document.getElementById("canvas-line-color");
+    }
+
+    if (document.getElementById("canvas-line-width")) {
+      this.lineWidthElt = document.getElementById("canvas-line-width");
+    }
+
+    if (document.getElementById("canvas-cleaner")) {
+      this.cleanerElt = document.getElementById("canvas-cleaner");
+    }
+  }
+
+  setListeners() {
     if (this.canvasElt) {
  
      this.canvasElt.addEventListener("mousedown", this.moveInCanvas.bind(this, "mouse"));
@@ -332,9 +378,7 @@ class Canvas {
      this.canvasElt.addEventListener("touchend", this.stopDrawing.bind(this));
      this.canvasElt.addEventListener("touchcancel", this.stopDrawing.bind(this));
     }
-   }
 
-  setOptionsListeners() {
     if (this.lineWidthElt) {
       this.lineWidthElt.addEventListener("input", this.setLineWidth.bind(this));
     }
@@ -348,9 +392,9 @@ class Canvas {
     }
   }
 
-  /*******************************/
-  /********** LISTENERS **********/
-  /*******************************/
+  /************************************/
+  /********** EVENTS METHODS **********/
+  /************************************/
 
   /**
    * @param {string} type
@@ -361,17 +405,17 @@ class Canvas {
     switch (type) {
 
       case "mouse":
-        this.getMouseLocation(event);
+        this.setMouseLocation(event);
         this.moveTo(event, this.mouseX, this.mouseY);
         break;
 
       case "touch":
-        this.getTouchLocation(event);
+        this.setTouchLocation(event);
         this.moveTo(event, this.touchX, this.touchY);
         break;
 
       default:
-        alert("Event type must be \"mouse\" or \"touch\"!");
+        alert("Event type must be mouse or touch!");
     }
   }
 
@@ -384,17 +428,17 @@ class Canvas {
       switch (type) {
 
         case "mouse":
-          this.getMouseLocation(event);
+          this.setMouseLocation(event);
           this.lineTo(this.mouseX, this.mouseY);
           break;
 
         case "touch":
-          this.getTouchLocation(event);
+          this.setTouchLocation(event);
           this.lineTo(this.touchX, this.touchY);
           break;
 
         default:
-          alert("Event type must be \"mouse\" or \"touch\"!");
+          alert("Event type must be mouse or touch!");
       }
     }
   }
@@ -415,6 +459,27 @@ class Canvas {
   clearCanvas() {
     this.context.clearRect(0, 0, this.canvasElt.width, this.canvasElt.height);
     this.endState = false;
+  }
+
+  /**************************************/
+  /********** LOCATION SETTERS **********/
+  /**************************************/
+
+  /**
+   * @param {object} event
+   */
+  setMouseLocation(event) {
+    this.mouseX = event.offsetX;
+    this.mouseY = event.offsetY;
+  }
+
+  /**
+   * @param {object} event
+   */
+  setTouchLocation(event) {
+    let position  = event.target.getBoundingClientRect();
+    this.touchX   = event.targetTouches[0].clientX - position.left;
+    this.touchY   = event.targetTouches[0].clientY - position.top;
   }
 
   /*************************************/
@@ -529,4 +594,4 @@ class Ajax {
 }
 
 /*! Author: Philippe Beck <philippe@philippebeck.net>
- Updated: 11th Apr 2021 @ 12:38:36 AM */
+ Updated: 11th Apr 2021 @ 12:58:23 PM */
