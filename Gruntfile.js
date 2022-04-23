4a0c937d0f5147ec8b70dd46821229d5287b8405
Philippe Beck <philippe@philippebeck.net>
2022-04-23 23:00:37 +0200

    Update gruntfile for new changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,41 +1,31 @@
 module.exports = function(grunt) {
-    grunt.initConfig({
-        pkg: grunt.file.readJSON("package.json"),
-        concat: {
-            options: {
-                banner: "/*! <%= pkg.name %> v<%= pkg.version %> | <%= pkg.homepage %> | <%= pkg.license %> License */\n\n", 
-                footer: "\n/*! Author: <%= pkg.author.name %> <<%= pkg.author.email %>>\n Updated: <%= grunt.template.today('dS mmm yyyy @ h:MM:ss TT') %> */"},
-            css: {
-                src: ["node_modules/normalize.css/normalize.css", "src/css/animadio.css"], 
-                dest: "dist/animadio.css"},
-            js: {
-                src: ["src/js/Animadio.js", "src/js/Slider.js", "src/js/Canvas.js", "src/js/Ajax.js"],
-                dest: "dist/animadio.js"}
-            },
-        postcss: {
-            options: {
-                processors: [require("autoprefixer")({overrideBrowserslist: "defaults"})]},
-            css: { src: "dist/animadio.css" }
-        },
-        cssmin: {
-            target: {
-                files: [
-                    { "dist/animadio.min.css": ["dist/animadio.css"] }
-                ]
-            }
-        },
-        uglify: {
-            target: {
-                files: {
-                  "dist/animadio.min.js": ["dist/animadio.js"]
-                }
-            }
-        }
-    });
-    
-    grunt.loadNpmTasks("grunt-contrib-concat");
-    grunt.loadNpmTasks("grunt-postcss");
-    grunt.loadNpmTasks("grunt-contrib-cssmin");
-    grunt.loadNpmTasks("grunt-contrib-uglify");
-    grunt.registerTask("default", ["concat", "postcss", "cssmin", "uglify"]);
+  grunt.initConfig({
+    pkg: grunt.file.readJSON("package.json"),
+    concat: {
+      options: {
+        banner: "/*! <%= pkg.name %> v<%= pkg.version %> | <%= pkg.homepage %> | <%= pkg.license %> License */\n\n", 
+        footer: "\n/* Author: <%= pkg.author.name %> <<%= pkg.author.email %>>\n Updated: <%= grunt.template.today('dS mmm yyyy @ h:MM:ss TT') %> */"
+      },
+      css: {
+        src: ["node_modules/normalize.css/normalize.css", "src/css/animadio.css"], 
+        dest: "dist/animadio.css"
+      }
+    },
+    postcss: {
+      options: { processors: [require("autoprefixer")({overrideBrowserslist: "defaults"})] },
+      css: { src: "dist/animadio.css" }
+    },
+    cssmin: {
+      target: {
+        files: [
+          { "dist/animadio.min.css": ["dist/animadio.css"] }
+        ]
+      }
+    }
+  });
+  
+  grunt.loadNpmTasks("grunt-contrib-concat");
+  grunt.loadNpmTasks("grunt-postcss");
+  grunt.loadNpmTasks("grunt-contrib-cssmin");
+  grunt.registerTask("default", ["concat", "postcss", "cssmin"]);
 };
